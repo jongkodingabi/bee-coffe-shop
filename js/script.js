b2d53208840c6bccfeb866330bb5a855bb320dca
jongkodingabi <abisamhazim04@gmail.com>
2024-10-06 09:16:47 +0700

    pengoptimalan web

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-//Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+//Toggle class active for hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-//Klik di luar sidebar untuk menghilangkan Nav
-const hamburger = document.querySelector('#hamburger-menu');
+//Toggle class active for search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//Klik di Elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
